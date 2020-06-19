80ecc7bfe29713c5c2b797649c43ea1d6df29dcf
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2020-06-19 11:46:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-module.exports = {
-    consumer_key:   'aAmKettjrYG6gOw9KFgBuMxz6',
-    consumer_secret:    '5CllpHYFb8UYlTJDsEyoW5d5HyWrjB2aIeXa5n8HVIRg4OPnL2',
-    access_token:   '1273216852223614977-LGQ1mmlXc0JprtCt0kFk5YkBI8zfLP',
-    access_token_secret:    '0zzJiJuYxjDFkMhJVmYUT0XDYXLTQDQQdU8LXM6K70bEV'
 
-}
